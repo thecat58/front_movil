4f1f27393e01ca71da40f5db6ec1d233e9e67374
chavito07 <kb3223739115@gmail.com>
2024-03-01 16:10:51 -0500

    vista registrar taller

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:movil/pages/nav.dart'; // Asegúrate de importar el archivo donde se define CustomBottomNavigationBar
+import 'package:movil/pages/nav.dart'; 
 
 void main() {
   runApp(MainApp()); // No es necesario el constante para MainApp
 }
 
 class MainApp extends StatelessWidget {
-  MainApp({Key? key}) : super(key: key); // Debes corregir la declaración del constructor
+  MainApp({Key? key}) : super(key: key); 
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( // Elimina el constante para MaterialApp
+    return MaterialApp( 
       debugShowCheckedModeBanner: false, // Oculta la marca de depuración
       home: Scaffold(
         appBar: AppBar(
---
+++ lib/pages/taller/registrar_taller.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const SubirDatos());
+
+class SubirDatos extends StatelessWidget {
+  const SubirDatos({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 205, 82, 69),
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.of(context).pop(); // Devuelve la página hacia atrás
+            },
+          ),
+          title: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Image.asset(
+                  'assets/logo.png',
+                  width: 50,
+                  height: 50,
+                ),
+              ),
+              SizedBox(height: 8.0),
+            ],
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 16.0),
+              Text(
+                'REGISTRAR TALLER',
+                style: TextStyle(color: Colors.black87),
+              ),
+              SizedBox(height: 8.0),
+              InkWell(
+                onTap: () {
+                  print('Botón de subir imagen presionado');
+                },
+                child: Container(
+                  width: 290.0,
+                  height: 150.0,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 205, 82, 69),
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.file_upload,
+                        color: Colors.black,
+                        size: 60.0,
+                      ),
+                      SizedBox(height: 8.0),
+                      Text(
+                        'Subir imagen del taller',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Container(
+                width: 290.0,
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        print('Botón Nombre del taller presionado');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        textStyle: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                        shadowColor: Colors.black,
+                        elevation: 4,
+                        minimumSize: Size(double.infinity, 50.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      child: Text('Nombre del taller', style: TextStyle(color: Colors.black)),
+                    ),
+                    SizedBox(height: 8.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        print('Botón Categoría presionado');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        textStyle: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                        shadowColor: Colors.black,
+                        elevation: 4,
+                        minimumSize: Size(double.infinity, 50.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      child: Text('Categoría', style: TextStyle(color: Colors.black)),
+                    ),
+                    SizedBox(height: 8.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        print('Botón Descripción del taller presionado');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        textStyle: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                        shadowColor: Colors.black,
+                        elevation: 4,
+                        minimumSize: Size(double.infinity, 50.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      child: Text('Descripción del taller', style: TextStyle(color: Colors.black)),
+                    ),
+                    SizedBox(height: 16.0),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Contactos',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
