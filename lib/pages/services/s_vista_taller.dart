ccfa9f6fff6efdaf0c87a73d183cbc49205edfc2
chavito07 <kb3223739115@gmail.com>
2024-04-03 16:18:11 -0500

    botones de elimar y actualizar sin funciones

--- lib/pages/services/s_vista_taller.dart
+++ lib/pages/services/s_vista_taller.dart
@@ -1,11 +1,14 @@
-import 'package:movil/pages/models/vista_taller.dart'; // Importación del modelo VistaTallerService
+import 'package:movil/pages/models/vista_taller.dart';
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 
 // Servicio para realizar operaciones relacionadas con los servicios de vista de taller.
 class VistaVistaTallerServiceService {
   static const String root = 'http://192.168.137.1:8000/api/taller/';
   static const String getAllAction = 'GET_ALL';
+  static const String updateEmpAction = 'UPDATE_EMP';
+  static const String deleteEmpAction = 'DELETE_EMP';
 
   // Obtiene una lista de servicios de vista de taller del servidor.
   static Future<List<VistaTallerService>> getVistaTallerServices() async {
@@ -27,5 +30,43 @@ class VistaVistaTallerServiceService {
   static List<VistaTallerService> parseResponse(String responseBody) {
     final parsed = json.decode(responseBody);
     return parsed.map<VistaTallerService>((json) => VistaTallerService.fromJson(json)).toList();
+  }
+
+  // Actualiza un taller en el servidor por su ID.
+  static Future<bool> updateTaller(String empId, File foto, String nombre, String ubicacion, String descripcion) async {
+    try {
+      var request = http.MultipartRequest('PUT', Uri.parse('$root$empId/'));
+      request.fields['action'] = updateEmpAction;
+      request.fields['nombre'] = nombre;
+      request.fields['ubicacion'] = ubicacion;
+      request.fields['descripcion'] = descripcion;
+
+      if (foto != null) {
+        var stream = http.ByteStream(Stream.castFrom(foto.openRead()));
+        var length = await foto.length();
+        var multipartFile = http.MultipartFile(
+          'foto',
+          stream,
+          length,
+          filename: foto.path.split('/').last,
+        );
+        request.files.add(multipartFile);
+      }
+
+      var response = await request.send();
+      return response.statusCode == 200;
+    } catch (e) {
+      throw Exception('Error al actualizar taller: $e');
+    }
+  }
+
+  // Elimina un taller del servidor por su ID.
+  static Future<bool> deleteTaller(String empId) async {
+    try {
+      final response = await http.delete(Uri.parse('$root$empId/?action=$deleteEmpAction'));
+      return response.statusCode == 200;
+    } catch (e) {
+      throw Exception('Error al eliminar taller: $e');
+    }
   }
 }
--- lib/pages/taller/vista_taller.dart
+++ lib/pages/taller/vista_taller.dart
@@ -100,79 +100,121 @@ class _VistaTallerState extends State<VistaTaller> {
     );
   }
 
-Widget _buildSmallCard(String imageUrl, String title, String ubicacion, String descripcion, BuildContext context) {
-  return GestureDetector(
-    onTap: () {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => VistaCategorias()),
-      );
-    },
-    child: Container(
-      width: double.infinity,
-      padding: EdgeInsets.all(10.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15.0),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 2,
-            blurRadius: 3,
-            offset: Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: double.infinity,
-            height: 200.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15.0),
-              image: DecorationImage(
-                image: imageUrl.isNotEmpty ? NetworkImage(imageUrl) : AssetImage('assets/placeholder_image.jpg') as ImageProvider<Object>,
-                fit: BoxFit.cover,
+  Widget _buildSmallCard(String imageUrl, String title, String ubicacion,
+      String descripcion, BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => VistaCategorias()),
+        );
+      },
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.0),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 3,
+              offset: Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              height: 200.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                image: DecorationImage(
+                  image: imageUrl.isNotEmpty
+                      ? NetworkImage(imageUrl)
+                      : AssetImage('assets/placeholder_image.jpg')
+                          as ImageProvider<Object>,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          SizedBox(height: 10.0),
-          Text(
-            title,
-            style: TextStyle(color: Colors.black, fontSize: 18.0, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 5.0),
-          Text(
-            ubicacion,
-            style: TextStyle(color: Colors.grey, fontSize: 14.0),
-          ),
-          SizedBox(height: 5.0),
-          Text(
-            descripcion,
-            style: TextStyle(color: Colors.grey, fontSize: 14.0),
-          ),
-        ],
+            SizedBox(height: 10.0),
+            Text(
+              title,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 5.0),
+            Text(
+              ubicacion,
+              style: TextStyle(
+                  color: const Color.fromARGB(255, 17, 17, 17), fontSize: 14.0),
+            ),
+            SizedBox(height: 5.0),
+            Text(
+              descripcion,
+              style: TextStyle(
+                  color: const Color.fromARGB(255, 17, 17, 17), fontSize: 14.0),
+            ),
+            SizedBox(height: 10.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment
+                  .center, // Alinea los botones hacia el centro
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Acción al presionar el botón "Actualizar"
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        Color.fromARGB(255, 205, 82, 69), // Color de fondo
+                  ),
+                  child: Text(
+                    'Actualizar',
+                    style: TextStyle(color: Colors.black), // Color del texto
+                  ),
+                ),
+                SizedBox(width: 50), // Ajusta el espacio entre los botones
+                ElevatedButton(
+                  onPressed: () {
+                    // Acción al presionar el botón "Eliminar"
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white, // Color de fondo
+                  ),
+                  child: Text(
+                    'Eliminar',
+                    style: TextStyle(color: Colors.black), // Color del texto
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-    ),
-  );
-}
-
-
-List<Widget> _buildTallerCards(List<VistaTallerService>? talleres, BuildContext context) {
-  if (talleres != null && talleres.isNotEmpty) {
-    return talleres.map((taller) {
-      return _buildSmallCard(taller.fotoUrl, taller.nombre, taller.ubicacion, taller.descripcion, context);
-    }).toList();
-  } else {
-    return [
-      Center(
-        child: Text('No hay talleres disponibles.'),
-      )
-    ];
+    );
   }
-}
 
+  List<Widget> _buildTallerCards(
+      List<VistaTallerService>? talleres, BuildContext context) {
+    if (talleres != null && talleres.isNotEmpty) {
+      return talleres.map((taller) {
+        return _buildSmallCard(taller.fotoUrl, taller.nombre, taller.ubicacion,
+            taller.descripcion, context);
+      }).toList();
+    } else {
+      return [
+        Center(
+          child: Text('No hay talleres disponibles.'),
+        )
+      ];
+    }
+  }
 
   void _clearSearchField(BuildContext context) {
     FocusScope.of(context).unfocus();
